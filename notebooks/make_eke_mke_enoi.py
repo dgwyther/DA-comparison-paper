d0b6c5c1c976e80d070b71801cb26db5cb94e352
dgwyther <david.gwyther@gmail.com>
2022-12-10 07:19:25 +1100

    energy and energy conv plots

---
+++ notebooks/make_eke_mke_enoi.py
@@ -0,0 +1,147 @@
+import xarray as xr
+from glob import glob
+import matplotlib.pyplot as plt
+import cartopy
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter
+from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes, zoomed_inset_axes
+import numpy as np
+import cmocean.cm as cmo
+from xgcm import Grid
+import matplotlib.gridspec as gridspec
+
+
+
+
+def processROMSGrid(ds):
+    ds = ds.rename({'eta_u': 'eta_rho', 'xi_v': 'xi_rho', 'xi_psi': 'xi_u', 'eta_psi': 'eta_v'})
+
+    coords={'X':{'center':'xi_rho', 'inner':'xi_u'}, 
+        'Y':{'center':'eta_rho', 'inner':'eta_v'}, 
+        'Z':{'center':'s_rho', 'outer':'s_w'}}
+
+    grid = Grid(ds, coords=coords, periodic=[])
+
+    if ds.Vtransform == 1:
+        Zo_rho = ds.hc * (ds.s_rho - ds.Cs_r) + ds.Cs_r * ds.h
+        z_rho = Zo_rho + ds.zeta * (1 + Zo_rho/ds.h)
+        Zo_w = ds.hc * (ds.s_w - ds.Cs_w) + ds.Cs_w * ds.h
+        z_w = Zo_w + ds.zeta * (1 + Zo_w/ds.h)
+    elif ds.Vtransform == 2:
+        Zo_rho = (ds.hc * ds.s_rho + ds.Cs_r * ds.h) / (ds.hc + ds.h)
+        z_rho = ds.zeta + (ds.zeta + ds.h) * Zo_rho
+        Zo_w = (ds.hc * ds.s_w + ds.Cs_w * ds.h) / (ds.hc + ds.h)
+        z_w = Zo_w * (ds.zeta + ds.h) + ds.zeta
+
+    ds.coords['z_w'] = z_w.where(ds.mask_rho, 0).transpose('ocean_time', 's_w', 'eta_rho', 'xi_rho')
+    ds.coords['z_rho'] = z_rho.where(ds.mask_rho, 0).transpose('ocean_time', 's_rho', 'eta_rho', 'xi_rho')
+    # Other Option is to transpose arrays and fill NaNs with a minimal depth
+    # ds['z_rho'] = z_rho.transpose(*('time', 's_rho','yh','xh'),transpose_coords=False).fillna(hmin)
+    # ds['z_w'] = z_w.transpose(*('time', 's_w','yh','xh'),transpose_coords=False).fillna(hmin)
+    ds.coords['z_rho0'] = z_rho.mean(dim='ocean_time')
+
+     # interpolate depth of levels at U and V points
+    ds['z_u'] = grid.interp(ds['z_rho'], 'X', boundary='fill')
+    ds['z_v'] = grid.interp(ds['z_rho'], 'Y', boundary='fill')
+
+    ds['pm_v'] = grid.interp(ds.pm, 'Y')
+    ds['pn_u'] = grid.interp(ds.pn, 'X')
+    ds['pm_u'] = grid.interp(ds.pm, 'X')
+    ds['pn_v'] = grid.interp(ds.pn, 'Y')
+    ds['pm_psi'] = grid.interp(grid.interp(ds.pm, 'Y'),  'X') # at psi points (eta_v, xi_u) 
+    ds['pn_psi'] = grid.interp(grid.interp(ds.pn, 'X'),  'Y') # at psi points (eta_v, xi_u)
+
+    ds['dx'] = 1/ds.pm
+    ds['dx_u'] = 1/ds.pm_u
+    ds['dx_v'] = 1/ds.pm_v
+    ds['dx_psi'] = 1/ds.pm_psi
+
+    ds['dy'] = 1/ds.pn
+    ds['dy_u'] = 1/ds.pn_u
+    ds['dy_v'] = 1/ds.pn_v
+    ds['dy_psi'] = 1/ds.pn_psi
+
+    ds['dz'] = grid.diff(ds.z_w, 'Z', boundary='fill')
+    ds['dz_w'] = grid.diff(ds.z_rho, 'Z', boundary='fill')
+    ds['dz_u'] = grid.interp(ds.dz, 'X')
+    ds['dz_w_u'] = grid.interp(ds.dz_w, 'X')
+    ds['dz_v'] = grid.interp(ds.dz, 'Y')
+    ds['dz_w_v'] = grid.interp(ds.dz_w, 'Y')
+
+    ds['dA'] = ds.dx * ds.dy
+
+    return ds
+
+def makeROMSGridObject(gridIn):
+    gridOut = Grid(gridIn, 
+    coords={'X':{'center':'xi_rho', 'inner':'xi_u'}, 
+    'Y':{'center':'eta_rho', 'inner':'eta_v'}, 
+    'Z':{'center':'s_rho', 'outer':'s_w'}},
+    periodic=False, 
+    metrics = {
+        ('X',): ['dx', 'dx_u', 'dx_v', 'dx_psi'], # X distances
+        ('Y',): ['dy', 'dy_u', 'dy_v', 'dy_psi'], # Y distances
+        ('Z',): ['dz', 'dz_u', 'dz_v', 'dz_w', 'dz_w_u', 'dz_w_v'], # Z distances
+        ('X', 'Y'): ['dA'] # Areas
+    })
+    return gridOut
+
+def load_roms(filename,overlap):
+    chunks = {'ocean_time': 1}
+    glb_files = sorted(glob(filename))
+    
+    def preprocessRemoveOverlap(ds):
+        '''remove the overlap from each file'''
+        return ds.isel(ocean_time = slice(0,-overlap))
+
+    for files in glb_files: 
+        print(files)
+        
+    ds = xr.open_mfdataset(glb_files, chunks=chunks, preprocess=preprocessRemoveOverlap, data_vars='minimal', compat='override', coords='minimal', parallel=False, join='right')
+    print('Loading data: OK!')
+    return ds
+
+print('load files')
+
+
+filename = '/srv/scratch/z3533092/DA-comparison-paper/notebooks/enoi_'
+ds = load_roms(filename='/srv/scratch/z3097808/forecasts_EnOI_TRADobs/output/eac_his_*.nc',overlap=19)
+
+ds = processROMSGrid(ds)
+grid = makeROMSGridObject(ds)
+
+
+
+print('load variables')
+# ds.u.load()
+# ds.v.load()
+
+print('calc mke')
+u_bar = grid.interp(ds.u.mean("ocean_time"),'X')
+v_bar = grid.interp(ds.v.mean("ocean_time"),'Y')
+mke = 0.5*(u_bar**2 + v_bar**2)
+
+# enoi["mke"] = mke
+
+u_rho = grid.interp(ds.u,'X')
+v_rho = grid.interp(ds.v,'Y')
+
+print('calc velocity anomalies')
+u_prime = u_rho - u_bar
+v_prime = v_rho - v_bar
+
+print('calc eke')
+eke = 0.5*(u_prime**2 + v_prime**2)
+
+eke_timemean = eke.mean(dim='ocean_time')
+
+# enoi["eke"] = eke
+
+print('saving eke time mean')
+eke_timemean.to_netcdf(filename+'eke_timemean.nc')
+print('saving mke')
+mke.to_netcdf(filename+'mke_full.nc')
+print('saving eke')
+eke.to_netcdf(filename+'eke_full.nc')
---
+++ notebooks/make_kmke_peke_enoi.py
@@ -0,0 +1,261 @@
+import xarray as xr
+from glob import glob
+import matplotlib.pyplot as plt
+import cartopy
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter
+from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes, zoomed_inset_axes
+import numpy as np
+import cmocean.cm as cmo
+from xgcm import Grid
+import matplotlib.gridspec as gridspec
+
+
+
+
+def processROMSGrid(ds):
+    ds = ds.rename({'eta_u': 'eta_rho', 'xi_v': 'xi_rho', 'xi_psi': 'xi_u', 'eta_psi': 'eta_v'})
+
+    coords={'X':{'center':'xi_rho', 'inner':'xi_u'}, 
+        'Y':{'center':'eta_rho', 'inner':'eta_v'}, 
+        'Z':{'center':'s_rho', 'outer':'s_w'}}
+
+    grid = Grid(ds, coords=coords, periodic=[])
+
+    if ds.Vtransform == 1:
+        Zo_rho = ds.hc * (ds.s_rho - ds.Cs_r) + ds.Cs_r * ds.h
+        z_rho = Zo_rho + ds.zeta * (1 + Zo_rho/ds.h)
+        Zo_w = ds.hc * (ds.s_w - ds.Cs_w) + ds.Cs_w * ds.h
+        z_w = Zo_w + ds.zeta * (1 + Zo_w/ds.h)
+    elif ds.Vtransform == 2:
+        Zo_rho = (ds.hc * ds.s_rho + ds.Cs_r * ds.h) / (ds.hc + ds.h)
+        z_rho = ds.zeta + (ds.zeta + ds.h) * Zo_rho
+        Zo_w = (ds.hc * ds.s_w + ds.Cs_w * ds.h) / (ds.hc + ds.h)
+        z_w = Zo_w * (ds.zeta + ds.h) + ds.zeta
+
+    ds.coords['z_w'] = z_w.where(ds.mask_rho, 0).transpose('ocean_time', 's_w', 'eta_rho', 'xi_rho')
+    ds.coords['z_rho'] = z_rho.where(ds.mask_rho, 0).transpose('ocean_time', 's_rho', 'eta_rho', 'xi_rho')
+    # Other Option is to transpose arrays and fill NaNs with a minimal depth
+    # ds['z_rho'] = z_rho.transpose(*('time', 's_rho','yh','xh'),transpose_coords=False).fillna(hmin)
+    # ds['z_w'] = z_w.transpose(*('time', 's_w','yh','xh'),transpose_coords=False).fillna(hmin)
+    ds.coords['z_rho0'] = z_rho.mean(dim='ocean_time')
+
+     # interpolate depth of levels at U and V points
+    ds['z_u'] = grid.interp(ds['z_rho'], 'X', boundary='fill')
+    ds['z_v'] = grid.interp(ds['z_rho'], 'Y', boundary='fill')
+
+    ds['pm_v'] = grid.interp(ds.pm, 'Y')
+    ds['pn_u'] = grid.interp(ds.pn, 'X')
+    ds['pm_u'] = grid.interp(ds.pm, 'X')
+    ds['pn_v'] = grid.interp(ds.pn, 'Y')
+    ds['pm_psi'] = grid.interp(grid.interp(ds.pm, 'Y'),  'X') # at psi points (eta_v, xi_u) 
+    ds['pn_psi'] = grid.interp(grid.interp(ds.pn, 'X'),  'Y') # at psi points (eta_v, xi_u)
+
+    ds['dx'] = 1/ds.pm
+    ds['dx_u'] = 1/ds.pm_u
+    ds['dx_v'] = 1/ds.pm_v
+    ds['dx_psi'] = 1/ds.pm_psi
+
+    ds['dy'] = 1/ds.pn
+    ds['dy_u'] = 1/ds.pn_u
+    ds['dy_v'] = 1/ds.pn_v
+    ds['dy_psi'] = 1/ds.pn_psi
+
+    ds['dz'] = grid.diff(ds.z_w, 'Z', boundary='fill')
+    ds['dz_w'] = grid.diff(ds.z_rho, 'Z', boundary='fill')
+    ds['dz_u'] = grid.interp(ds.dz, 'X')
+    ds['dz_w_u'] = grid.interp(ds.dz_w, 'X')
+    ds['dz_v'] = grid.interp(ds.dz, 'Y')
+    ds['dz_w_v'] = grid.interp(ds.dz_w, 'Y')
+
+    ds['dA'] = ds.dx * ds.dy
+
+    return ds
+
+def makeROMSGridObject(gridIn):
+    gridOut = Grid(gridIn, 
+    coords={'X':{'center':'xi_rho', 'inner':'xi_u'}, 
+    'Y':{'center':'eta_rho', 'inner':'eta_v'}, 
+    'Z':{'center':'s_rho', 'outer':'s_w'}},
+    periodic=False, 
+    metrics = {
+        ('X',): ['dx', 'dx_u', 'dx_v', 'dx_psi'], # X distances
+        ('Y',): ['dy', 'dy_u', 'dy_v', 'dy_psi'], # Y distances
+        ('Z',): ['dz', 'dz_u', 'dz_v', 'dz_w', 'dz_w_u', 'dz_w_v'], # Z distances
+        ('X', 'Y'): ['dA'] # Areas
+    })
+    return gridOut
+
+def load_roms(filename,overlap):
+    chunks = {'ocean_time': 1}
+    glb_files = sorted(glob(filename))
+    
+    def preprocessRemoveOverlap(ds):
+        '''remove the overlap from each file'''
+        return ds.isel(ocean_time = slice(0,-overlap))
+
+    for files in glb_files: 
+        print(files)
+        
+    ds = xr.open_mfdataset(glb_files, chunks=chunks, preprocess=preprocessRemoveOverlap, data_vars='minimal', compat='override', coords='minimal', parallel=False, join='right')
+    print('Loading data: OK!')
+    return ds
+
+def rho_eos(Tt,Ts,z_r):
+    #     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    # %
+    # % function [rho,bvf]=rho_eos(Tt,Ts,z_r)
+    # %
+    # % Computes density via Equation Of State (EOS) for seawater.
+    # % If so prescribed, non-linear EOS of Jackett and McDougall (1995)
+    # % is used.
+    # %
+    # % Tt potential temperature [deg Celsius].
+    # % Ts salinity [PSU].
+    # % Tz pressure/depth, [depth in meters and negative].
+    # %
+    # % K0, K1 and K2 are the pressure polynomial coefficients for secant
+    # % bulk modulus, so that
+    # %
+    # %               bulk = K0 - K1 * z + K2 * z**2 ;
+    # %
+    # % while rho1 is sea-water density [kg/m^3] at standard pressure
+    # % of 1 Atm, so that the density anomaly at in-sity pressure is
+    # %
+    # %               rho = rho1 / (1 + z / bulk) - 1000
+    # %
+    # %  Reference:
+    # %
+    # %  Jackett, D. R. and T. J. McDougall, 1995, Minimal Adjustment of
+    # %  Hydrostatic Profiles to Achieve Static Stability, Journ of Atmos.
+    # %  and Oceanic Techn., vol. 12, pp. 381-389.
+    # %
+    # % << This equation of state formulation has been derived by Jackett
+    # %    and McDougall (1992), unpublished manuscript, CSIRO, Australia.
+    # %    It computes in-situ density anomaly as a function of potential
+    # %    temperature (Celsius) relative to the surface, salinity (PSU),
+    # %    and depth (meters).  It assumes  no  pressure  variation along
+    # %    geopotential  surfaces,  that  is,  depth  and  pressure  are
+    # %    interchangeable. >>
+    # %                                          John Wilkin, 29 July 92
+    # %
+    # % Check Values: T=3 C S=35.5 PSU Z=-5000 m rho=1050.3639165364 
+    
+    import numpy as np
+    
+    A00=+19092.56;A01=+209.8925;
+    A02=-3.041638;A03=-1.852732e-3;A04=-1.361629e-5;A10=104.4077;
+    A11=-6.500517;A12=+0.1553190;A13=2.326469e-4;AS0=-5.587545;
+    AS1=+0.7390729;AS2=-1.909078e-2;B00=+4.721788e-1;B01=+1.028859e-2;
+    B02=-2.512549e-4;B03=-5.939910e-7;B10=-1.571896e-2;B11=-2.598241e-4;
+    B12=+7.267926e-6;BS1=+2.042967e-3;E00=+1.045941e-5;E01=-5.782165e-10;
+    E02=+1.296821e-7;E10=-2.595994e-7;E11=-1.248266e-9;E12=-3.508914e-9;
+
+    QR=+999.842594;Q01=+6.793952e-2;Q02=-9.095290e-3;
+    Q03=+1.001685e-4;Q04=-1.120083e-6;Q05=+6.536332e-9;Q10=+0.824493;
+    Q11=-4.08990e-3;Q12=+7.64380e-5;Q13=-8.24670e-7;Q14=+5.38750e-9;
+    QS0=-5.72466e-3;QS1=+1.02270e-4;QS2=-1.65460e-6;Q20=+4.8314e-4;
+    
+    
+    sqrtTs=np.sqrt(Ts);
+
+    K0=A00+Tt*(A01+Tt*(A02+Tt*(A03+Tt*A04)))+Ts*(A10+Tt*(A11+Tt*(A12+Tt*A13))+sqrtTs*(AS0+Tt*(AS1+Tt*AS2)));
+    K1=B00+Tt*(B01+Tt*(B02+Tt*B03))+Ts*(B10+Tt*(B11+Tt*B12)+sqrtTs*BS1);
+    K2=E00+Tt*(E01+Tt*E02)+Ts*(E10+Tt*(E11+Tt*E12));
+    rho1=QR+Tt*(Q01+Tt*(Q02+Tt*(Q03+Tt*(Q04+Tt*Q05))))+Ts*(Q10+Tt*(Q11+Tt*(Q12+Tt*(Q13+Tt*Q14)))+sqrtTs*(QS0+Tt*(QS1+Tt*QS2))+Ts*Q20);
+    rho=rho1/(1+0.1*z_r/(K0-z_r*(K1-z_r*K2)));
+    
+    return rho
+
+
+print('load files')
+
+
+filename = '/srv/scratch/z3533092/DA-comparison-paper/notebooks/enoi_'
+ds = load_roms(filename='/srv/scratch/z3097808/forecasts_EnOI_TRADobs/output/eac_his_*.nc',overlap=19)
+
+ds = processROMSGrid(ds)
+grid = makeROMSGridObject(ds)
+
+
+print('calc peke')
+
+def calc_PeKe(input):
+    
+    g = 9.81
+    print('calculate density with custom rho_eos function')
+    rho = rho_eos(input.temp,input.salt,input.z_rho)
+    print('save rho into ds')          
+    input["rho"] = rho # write to ds so we can use the mean function
+    input = input.drop_vars(['temp','salt','z_rho'])
+     # calculate time-means
+    print('calc time means')
+    rho_bar = input.rho.mean(dim='ocean_time')
+    w_bar = input.w.mean(dim='ocean_time')
+     # calculate primes
+    print('calc primes')
+    w_prime = input.w - w_bar
+    rho_prime = input.rho - rho_bar
+    input = input.drop_vars(['w','rho'])
+    w_prime_rho = grid.interp(w_prime,axis="Z",boundary="fill")
+    # plt.figure()
+    # w_prime_rho.mean(dim=['ocean_time','s_rho']).plot()
+    # plt.show()
+    # plt.figure()
+    # rho_prime.mean(dim=['ocean_time','s_rho']).plot()
+    # plt.show()
+    PeKe = -g*rho_prime*w_prime_rho
+    return PeKe
+
+peke = calc_PeKe(ds)
+peke_timemean = peke.mean(dim='ocean_time')
+
+print('calc kmke')
+
+def calc_KmKe(input):
+
+    u = input.u
+    v = input.v
+
+    u_bar = u.mean("ocean_time")
+    v_bar = v.mean("ocean_time")
+
+
+    u_prime = u - u_bar
+    v_prime = v - v_bar
+
+    u_bar_rho=grid.interp(u_bar,axis="X",boundary="fill")
+    v_bar_rho=grid.interp(v_bar,axis="Y",boundary="fill")
+
+    u_prime_rho=grid.interp(u_prime,axis="X",boundary="fill")
+    v_prime_rho=grid.interp(v_prime,axis="Y",boundary="fill")
+    
+    dudx = grid.derivative(u_bar,'X',boundary='fill')
+    dudy = grid.derivative(u_bar_rho,'Y',boundary='fill')
+    dvdx = grid.derivative(v_bar_rho,'X',boundary='fill')
+    dvdy = grid.derivative(v_bar,'Y',boundary='fill')
+
+    dudy = grid.interp(dudy,axis="Y",boundary="fill")
+    dvdx = grid.interp(dvdx,axis="X",boundary="fill")
+
+    rho0 = 1025
+
+    uu = (u_prime_rho*u_prime_rho).mean(dim='ocean_time')
+    uv = (u_prime_rho*v_prime_rho).mean(dim='ocean_time')
+    vu = (v_prime_rho*u_prime_rho).mean(dim='ocean_time')
+    vv = (v_prime_rho*v_prime_rho).mean(dim='ocean_time')
+
+
+    KmKe = -rho0*(uu*dudx + uv*dudy + vu*dvdx + vv*dvdy)
+    return KmKe
+
+kmke = calc_KmKe(ds)
+
+print('saving peke time mean')
+peke_timemean.to_netcdf(filename+'peke_timemean.nc')
+print('saving kmke')
+kmke.to_netcdf(filename+'kmke_full.nc')
+print('saving peke')
+peke.to_netcdf(filename+'peke_full.nc')
